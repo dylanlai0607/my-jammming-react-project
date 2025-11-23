9bd00401a292373da887135c81902e3e42e4e14d
Dylan Lai <dylanl67@bu.edu>
2025-11-23 10:28:09 -0500

    initialize components

---
+++ components/Track.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Track() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Track;
---
+++ components/Tracklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Tracklist() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Tracklist;
