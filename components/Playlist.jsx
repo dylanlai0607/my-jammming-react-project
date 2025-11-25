e33e5b4377c452ca3159fa691b2bd63767432d49
Dylan Lai <dylanl67@bu.edu>
2025-11-25 15:46:56 -0500

    Added playlist name state

--- components/Playlist.jsx
+++ components/Playlist.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Track from './Track.jsx'
 
 
 function Playlist(props) {
     const { playlist, setPlaylist } = props;
+    const [playListName, setPlaylistName] = useState("");
     const actionLabel = "-"
     
     function handleRemoveFromPlaylist(songInfo) {
         setPlaylist(prev => prev.filter(track => track.id !== songInfo.id));
+    }
+
+    function handlePlaylistNameChange(event) {
+        setPlaylistName(event.target.value);
     }
     
     return (
         <div className="playlist">
             <h2>Playlist</h2>
             <form>
-                <input type="text" placeholder="Enter playlist name"/>
+                <input type="text" placeholder="Enter playlist name" onChange={handlePlaylistNameChange} value={playListName} />
+                <button className="save-button">Save to Playlist</button>
             </form>
-            <button className="save-button">Save to Playlist</button>
+            
             
             <ul className="playlist-items">
                 {playlist.map((songInfo) => <li key={songInfo.id}><Track songInfo={songInfo} onAction={handleRemoveFromPlaylist} actionLabel={actionLabel}/></li>)}
